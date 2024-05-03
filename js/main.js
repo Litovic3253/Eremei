566b1e79b39cff8fb3be975c36703e516fd84d0d
Litovic3253 <121265055+Litovic3253@users.noreply.github.com>
2024-05-03 17:55:14 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -10,3 +10,16 @@ var swiper = new Swiper(".mySwiper", {
       clickable: true,
     },
   });
+
+  let menu = document.querySelector('.menu-icon');
+  let navbar = document.querySelector('.navbar');
+
+  menu.onclick = () => {
+    menu.classList.toggle("move");
+    navbar.classList.toggle("open-menu");
+  }
+
+  window.onscroll = () => {
+    menu.classList.remove("move");
+    navbar.classList.remove("open-menu");
+  }
